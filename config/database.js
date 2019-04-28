75f454844be676d6f86738c1a9ad81b16e94ade1
loghinfrancisc <loghinfrancisc@users.noreply.github.com>
2019-04-29 01:36:05 +0200

    update

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb+srv://mean:qaD4m5^&PS1zMy@cluster0-msowr.mongodb.net/meanauth?retryWrites=true',
+  database: 'mongodb+srv://mean:7gSGIlwwnkXV8q@cluster0-msowr.mongodb.net/meanauth?retryWrites=true',
   secret: 'yoursecret'
 }
