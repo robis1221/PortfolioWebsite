71fc0438e88ade390fed18d90418ed516cde1c26
robis1221 <robis1221@inbox.lv>
2019-05-15 19:17:01 +0200

    finalplease

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { jsonpFactory } from '@angular/http/src/http_module';
 import data from '../../../assets/image-tiles.json';
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -13,6 +12,7 @@ stringJson:string;
   saveJson(json:string){
   
     console.log(json);
+    alert("JSON has not been saved because sending it to back-end wasn't implemented yet. Redirecting to home");
   }
   ngOnInit() {
     this.stringJson=JSON.stringify(data,null,"\t");
